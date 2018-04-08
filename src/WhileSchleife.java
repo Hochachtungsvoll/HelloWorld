3a300fe11631111f09c4f7342369545f6b46ab12
Hochachtungsvoll <38181595+Hochachtungsvoll@users.noreply.github.com>
2018-04-08 13:54:06 +0200

    updäyt

---
+++ src/WhileSchleife.java
@@ -0,0 +1,16 @@
+public class WhileSchleife {
+
+        public static void main (String [] args){
+
+            int a = 10;
+            int b = 0;
+
+            while (a>b){
+                System.out.println("Countdown: " +a);
+
+                a--;
+            }
+
+            System.out.println("Ende!");
+        }
+}
---
+++ src/WhileSchleife2.java
@@ -0,0 +1,17 @@
+public class WhileSchleife2 {
+
+        public static void main (String [] args){
+
+            int a = 5;
+            int b = 0;
+
+            do {
+                System.out.println("Party!");
+                b++;
+            } while (a>b);
+
+            System.out.println("Ende!");
+
+        }
+
+}
