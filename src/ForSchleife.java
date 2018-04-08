eb63bed9d346352d7aad937ff16a4db76cc5aa0f
Hochachtungsvoll <38181595+Hochachtungsvoll@users.noreply.github.com>
2018-04-08 14:08:42 +0200

    neues

---
+++ src/ForSchleife.java
@@ -0,0 +1,13 @@
+public class ForSchleife {
+
+    public static void main(String[] args) {
+
+        int hallo = 10;
+        for (int i = 0; i < hallo; i++) {
+            System.out.println("Runde " + i);
+
+        }
+
+    }
+
+}
---
+++ src/ForSchleife2.java
@@ -0,0 +1,16 @@
+public class ForSchleife2 {
+
+    public static void main(String[] args) {
+
+        int hallo = 10;
+        for (int i = 1; i <=5; i++) {
+            for (int j = 1; j <= i; j++) {
+
+            System.out.println("ha");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
